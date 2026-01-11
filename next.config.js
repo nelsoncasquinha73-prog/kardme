acd3c90f10de3ff2bb697579825477f7037f0acc
Nelson Casquinha <nelsoncasquinha@Mac.lan>
2026-01-11 15:47:18 +0000

    Fix next.config.js with alias and images config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  domains: [
-    'via.placeholder.com',
-    'eu.chat-img.sintra.ai',
-    'apkfamihczgzyxplksua.supabase.co',
-    'apkfamihczgzyxplksua.storage.supabase.co',
-  ],
-},
+    domains: [
+      'via.placeholder.com',
+      'eu.chat-img.sintra.ai',
+      'apkfamihczgzyxplksua.supabase.co',
+      'apkfamihczgzyxplksua.storage.supabase.co',
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
