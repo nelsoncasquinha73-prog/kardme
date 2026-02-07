99e19a44fb49085091a28bd1834dbbf05e4b6000
Your Name <you@example.com>
2026-02-07 23:42:21 +0000

    Revert "debug: add console.log to block name helper"
    This reverts commit 47836f56ffe4482a43b5c7b3e8b7efc3ecf4262d.

--- components/editor/blockNameHelper.ts
+++ components/editor/blockNameHelper.ts
@@ -1,4 +1,3 @@
 export function getBlockName(type: string, t: (key: string) => string): string {
-  console.log('getBlockName called:', type, 'result:', t(`blocks.${type}`))
   return t(`blocks.${type}`) || type
 }
