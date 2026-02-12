1e67f4366ebcd0f71a8a5757650988037a7275ac
Your Name <you@example.com>
2026-02-12 19:55:40 +0000

    fix: Color import from text-style

--- components/editor/RichTextEditor.tsx
+++ components/editor/RichTextEditor.tsx
@@ -6,8 +6,7 @@ import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
 import TextAlign from '@tiptap/extension-text-align'
 import Link from '@tiptap/extension-link'
-import TextStyle from '@tiptap/extension-text-style'
-import Color from '@tiptap/extension-color'
+import { TextStyle, Color } from '@tiptap/extension-text-style'
 
 type Props = {
   value: string
