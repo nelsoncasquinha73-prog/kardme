53c6961a7ffa39a1e586b75e4c753355728175b9
Your Name <you@example.com>
2026-04-09 09:50:08 +0100

    fix: add createPortal import to GalleryBlock

--- components/blocks/GalleryBlock.tsx
+++ components/blocks/GalleryBlock.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
 import useEmblaCarousel from 'embla-carousel-react'
 import AutoplayImport from 'embla-carousel-autoplay'
 
